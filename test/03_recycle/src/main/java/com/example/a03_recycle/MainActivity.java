58658b9206e8435064e4bace07d085e2b39dbbfe
唐隆辉 <1776234240@qq.com>
2023-05-14 00:23:52 +0800

    动画 test

--- test/03_recycle/src/main/java/com/example/a03_recycle/MainActivity.java
+++ test/03_recycle/src/main/java/com/example/a03_recycle/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.a03_recycle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnTest;
+    private Button btnTest2;
     private RecyclerView rvList;
     private List<String> mDate;
 
@@ -25,16 +30,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDate = new ArrayList();
-        for (int i = 0; i < 10; i++) {
-            mDate.add(new String("" + i));
-        }
-
+        mDate = getData(5);
         btnTest = (Button) findViewById(R.id.btn_test);
+        btnTest2 = (Button) findViewById(R.id.btn_test2);
         rvList = (RecyclerView) findViewById(R.id.rv_list);
         adapter = new TestAdapter(mDate, this);
         rvList.setAdapter(adapter);
-        rvList.setLayoutManager(new LinearLayoutManager(this));
+        GridLayoutManager gridLayoutManager =new GridLayoutManager(this,4);
+
+        rvList.setLayoutManager(gridLayoutManager);
 
         show=false;
 
@@ -45,5 +49,24 @@ public class MainActivity extends AppCompatActivity {
                 adapter.notifyDataSetChanged();
             }
         });
+
+        btnTest2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adapter.setDate(getData(new Random().nextInt(12)));
+                Animation animation= new AlphaAnimation(0,1);
+                animation.setDuration(3000);
+                rvList.startAnimation(animation);
+            }
+        });
+    }
+
+    private List getData(int num) {
+        List mDate = new ArrayList();
+        for (int i = 0; i < num; i++) {
+            mDate.add(new String("" + i));
+        }
+
+        return mDate;
     }
 }
--- test/03_recycle/src/main/java/com/example/a03_recycle/TestAdapter.java
+++ test/03_recycle/src/main/java/com/example/a03_recycle/TestAdapter.java
@@ -5,6 +5,11 @@ import android.graphics.Color;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -27,6 +32,12 @@ public class TestAdapter extends RecyclerView.Adapter {
         this.mShow = mShow;
     }
 
+    public void setDate(List mDate) {
+        this.mDate.clear();
+        this.mDate.addAll(mDate);
+        notifyDataSetChanged();
+    }
+
     public boolean ismShow() {
         return mShow;
     }
@@ -39,7 +50,8 @@ public class TestAdapter extends RecyclerView.Adapter {
         RelativeLayout.LayoutParams layoutParams =
                 new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         layout.setBackgroundColor(Color.BLUE);
-        layoutParams.topMargin = 20;
+//        layoutParams.topMargin = 20;
+        layoutParams.setMargins(20,20,20,20);
         layout.setLayoutParams(layoutParams);
 
         ImageView view = new ImageView(mActivity);
@@ -62,6 +74,48 @@ public class TestAdapter extends RecyclerView.Adapter {
         viewHolder.addView.setVisibility(mShow ? View.VISIBLE : View.INVISIBLE);
         Log.e("tlh","position"+position);
 //        holder.itemView.setVisibility(View.VISIBLE);
+        Animation animation= new AlphaAnimation(0,1);
+        animation.setDuration(3000);
+
+        Animation scaleAnimation = getAnimation();
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                Log.e("tlh","onAnimationStart");
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Log.e("tlh","onAnimationEnd");
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                Log.e("tlh","onAnimationEnd");
+            }
+        });
+
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.itemView.startAnimation(animation);
+            }
+        });
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return false;
+            }
+        });
+    }
+
+    @NonNull
+    private Animation getAnimation() {
+        Animation scaleAnimation=new ScaleAnimation(0,0,1,1);
+        scaleAnimation.setDuration(3000);
+        return scaleAnimation;
     }
 
     @Override
