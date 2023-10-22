39abc2c8ef2cbc1ebafa386796d7e73062d93d14
唐隆辉 <1776234240@qq.com>
2023-10-22 21:45:23 +0800

    安卓获取系统属性

--- app/src/main/java/com/example/accumulate/MainActivity.java
+++ app/src/main/java/com/example/accumulate/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.accumulate;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.lib_common.unit.SystemPropertyUtil;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * adb shell setprop "s" "1"
+     * adb shell getprop "s"
+     */
+    public void propTest(View view) throws IOException {
+//        String value=PropUnit.PropUnit.getProperty("s","0");
+//        Properties properties = new Properties();
+//        String value = System.getProperties("s");
+        String value = SystemPropertyUtil.get(this, "s", "0");
+        Toast.makeText(this, "s :" + value, Toast.LENGTH_LONG).show();
     }
 }
