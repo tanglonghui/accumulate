dfc88c9a9ef801f4864ba20df3fa9a4c89c68dc0
唐隆辉 <1776234240@qq.com>
2024-04-17 22:34:32 +0800

    线程测试

--- test/10_thread_state/src/main/java/com/example/a10_thread_state/MainActivity.java
+++ test/10_thread_state/src/main/java/com/example/a10_thread_state/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.ConditionVariable;
+import android.os.Trace;
 import android.util.Log;
 import android.view.View;
 
