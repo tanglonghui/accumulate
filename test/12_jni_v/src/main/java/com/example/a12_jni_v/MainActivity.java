4f9d33c9addf67e4dc96a1a2ab8693c4125cb1c5
唐隆辉 <1776234240@qq.com>
2025-02-08 00:31:44 +0800

    jni-v base

---
+++ test/12_jni_v/src/main/java/com/example/a12_jni_v/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.a12_jni_v;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
